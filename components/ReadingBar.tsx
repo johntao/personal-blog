d206ee1422f80af1009dc211ede9ff04d98cad9a
Tao-Zonyo <zytao28@icloud.com>
2022-08-22 22:46:26 +0800

    minor: refactor ReadingBar - add summary for previous post
    feat: new post reading-progress-bar

--- components/ReadingBar.tsx
+++ components/ReadingBar.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 export default function ReadingBar() {
   const [width, setWidth] = useState(0);
-  const scrollHeight = () => {
+  const updateWidthOnScroll = () => {
     // const de = document.documentElement
     const db = document.body
     //v1: const el = target.current;
@@ -13,14 +13,14 @@ export default function ReadingBar() {
     //v2: const totalY = (de.scrollHeight || db.scrollHeight) - de.clientHeight;
     //v3: const totalY = db.scrollHeight - window.innerHeight;
     const totalY = db.scrollHeight - window.innerHeight;
-    if(!curY || !totalY) return 0
+    if(!curY || !totalY) return setWidth(0)
     //v3: setWidth(Number((curY / totalY).toFixed(2)) * 100);
     if (curY > totalY) return setWidth(100)
     setWidth((curY / totalY) * 100);
   }
   useEffect(() => {
-    window.addEventListener("scroll", scrollHeight);
-    return () => window.removeEventListener("scroll", scrollHeight);
+    window.addEventListener("scroll", updateWidthOnScroll);
+    return () => window.removeEventListener("scroll", updateWidthOnScroll);
   }, []);
   return <div className="fixed h-1 z-30 bg-gradient-to-r from-cyan-500 to-blue-500" style={{ width: `${width}%` }} />;
 }
